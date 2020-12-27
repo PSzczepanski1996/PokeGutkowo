42df549e389f47b377861d8b725c80e1690219f0
HoshiYamazaki <hoshiyamazaki01@gmail.com>
2020-12-28 00:36:01 +0100

    Fix bugs, tbh going to sleep

--- pokegutkowo/urls.py
+++ pokegutkowo/urls.py
@@ -1,10 +1,10 @@
 """PokeGutkowo app urls."""
 from django.urls import path
 
-from pokegutkowo.views import PlayersView
+from pokegutkowo.views import PlayersView, PostsView, SettingsView
 
 urlpatterns = [
-    path('players/', PlayersView.as_view(), 'players_view'),
-    path('posts/', PlayersView.as_view(), 'players_view'),
-    path('players/', PlayersView.as_view(), 'players_view'),
+    path('players/', PlayersView.as_view(), name='players_view'),
+    path('posts/', PostsView.as_view(), name='posts_view'),
+    path('settings/', SettingsView.as_view(), name='settings_view'),
 ]
