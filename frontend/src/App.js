f1d5731d808eb17c0f3115c206e81bfc9b927058
PSzczepanski1996 <p.szczepanski996@gmail.com>
2020-12-28 19:00:17 +0100

    Fix app not being responsive on mobiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import {
     Parser as HtmlToReactParser,
@@ -234,6 +235,9 @@ class Website extends Component {
             <div>
                 <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                     <a className='navbar-brand' href="/"><Title/></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                     <ul className='navbar-nav mr-auto'>
                             <li className='nav-item'>
